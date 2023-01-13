718f35f90479e828d091abb21c9db3f4149b05f3
Ahmad <1ahmadkamal1@gmail.com>
2023-01-13 01:17:43 -0500

    multilateration passes all tests

--- c_code/main.c
+++ c_code/main.c
@@ -6,5 +6,6 @@ int main() {
     bool ring_buffer_pass = test_ring_buffer_read_and_replace();
     bool complex_pass = test_complex_type();
     bool fourier_pass = test_fourier();
+    bool multilateration_pass = test_multilateration();
     return 0;
 }
--- c_code/unit_tests.c
+++ c_code/unit_tests.c
@@ -146,3 +146,127 @@ bool test_fourier(){
 
     return all_tests_pass;
 }
+
+bool test_multilateration(){
+    /* Note that it is expected that multilateration won't work if:
+     * - Any 2D distance is 0 (ie ROV directly over a node)
+     * - The 3 stationary nodes are collinear (in 2D)
+     */
+    // Test in the following coordinates
+    coord_3d_t origin_coord = {0, 0, 0};
+    coord_3d_t stationary_1_coord = {1, 0, 0};
+    coord_3d_t stationary_2_coords[3] = {{3, 2, -2}, {-1, 4, 2}, {0, 3, -2}}; // 1st, 2nd quadrant, and y axis
+    coord_3d_t stationary_3_coords[4] = {{2, 5, -4}, {-4, 1, 0}, {-7, -2, 2}, {2, 5, -1}}; // each quadrant
+    coord_3d_t rov_coords[5] = {{0, 0, 0}, {1, 1, 1}, {-1, 1, 2}, {-1, -1, -1}, {1, -1, -2}}; // center and each quadrant
+    float og_depth = -5;
+
+    bool quadrant_tests_pass = true;
+
+    for (uint8_t idx_stationary_2_coord = 0; idx_stationary_2_coord < 3; idx_stationary_2_coord++){
+        for (uint8_t idx_stationary_3_coord = 0; idx_stationary_3_coord < 4; idx_stationary_3_coord++){
+            for (uint8_t idx_rov_coord = 0; idx_rov_coord < 5; idx_rov_coord++){
+                coord_3d_t coords_3d[N_ALL_NODES] = {
+                        origin_coord,
+                        stationary_1_coord,
+                        stationary_2_coords[idx_stationary_2_coord],
+                        stationary_3_coords[idx_stationary_3_coord],
+                };
+                coord_3d_t rov_coord = rov_coords[idx_rov_coord];
+                float curr_depth = og_depth + rov_coord.z;
+
+                float dists_3d[N_ALL_NODES] = {0};
+                for (uint8_t i = 1; i < N_ALL_NODES; ++i){
+                    dists_3d[i] = dist_between_points_3d(rov_coord, coords_3d[i]);
+                }
+
+                coord_3d_t position = multilaterate(coords_3d, dists_3d, curr_depth, og_depth);
+
+                bool test_passed = (fabsf(position.x - rov_coord.x) < 1e-5
+                                    && fabsf(position.y - rov_coord.y) < 1e-5
+                                    && fabsf(position.z - rov_coord.z) < 1e-5);
+                if(!test_passed){
+                    quadrant_tests_pass = false;
+                    printf("coords_3d: (%.2f, %.2f, %.2f), (%.2f, %.2f, %.2f), (%.2f, %.2f, %.2f), (%.2f, %.2f, %.2f)\n",
+                           coords_3d[0].x, coords_3d[0].y, coords_3d[0].z,
+                           coords_3d[1].x, coords_3d[1].y, coords_3d[1].z,
+                           coords_3d[2].x, coords_3d[2].y, coords_3d[2].z,
+                           coords_3d[3].x, coords_3d[3].y, coords_3d[3].z);
+                    printf("dists3d: %.2f, %.2f, %.2f, estimate: (%.2f, %.2f, %.2f), actual: (%.2f, %.2f, %.2f)\n\n",
+                           dists_3d[1], dists_3d[2], dists_3d[3],
+                           position.x, position.y, position.z,
+                           rov_coord.x, rov_coord.y, rov_coord.z);
+                }
+            }
+        }
+    }
+    if (quadrant_tests_pass){
+        printf("Multilateration quadrant tests Passed\n");
+    } else {
+        printf("Multilateration quadrant tests Failed\n");
+    }
+
+    /* for completeness, test that edge cases won't break the code */
+    // if the stationary nodes are collinear, it is assumed that the ROV is in one of the two options (positive)
+    coord_3d_t collinear_stationary_coords[N_ALL_NODES] = {
+            {0, 0, 0},
+            {1, 0, 0},
+            {2, 0, 0},
+            {3, 0, 0},
+    };
+    float collinear_stationary_dists_3d[N_ALL_NODES] = {0, 1, sqrtf(2), sqrtf(5)}; // ie ROV at (1, 1, 0) or (1, -1, 0)
+    coord_3d_t collinear_stationary_estimated_position = multilaterate(collinear_stationary_coords, collinear_stationary_dists_3d, -5, -5);
+    bool stationary_collinear_test_passed = (fabsf(collinear_stationary_estimated_position.x - 1) < 1e-5
+                                  && fabsf(collinear_stationary_estimated_position.y - 1) < 1e-5
+                                  && fabsf(collinear_stationary_estimated_position.z - 0) < 1e-5);
+    if (stationary_collinear_test_passed){
+        printf("Multilateration stationary collinear tests Passed\n");
+    } else {
+        printf("Multilateration stationary collinear tests Failed\n");
+    }
+
+    // if ROV collinear with nodes, should not cause an issue
+    coord_3d_t collinear_rov_coords[N_ALL_NODES] = {
+            {0, 0, 0},
+            {1, 0, 0},
+            {2, 0, 0},
+            {3, 4, 0},
+    };
+    float rov_collinear_dists_3d[N_ALL_NODES] = {0, 1, 2, 5};  // ie ROV at (0, 0, 0)
+    coord_3d_t rov_collinear_estimated_position = multilaterate(collinear_rov_coords, rov_collinear_dists_3d, -5, -5);
+    bool rov_collinear_test_passed = (fabsf(rov_collinear_estimated_position.x - 0) < 1e-5
+                                      && fabsf(rov_collinear_estimated_position.y - 0) < 1e-5
+                                      && fabsf(rov_collinear_estimated_position.z - 0) < 1e-5);
+    if (rov_collinear_test_passed){
+        printf("Multilateration ROV collinear tests Passed\n");
+    } else {
+        printf("Multilateration ROV collinear tests Failed\n");
+    }
+
+    // if ROV on top of nodes
+    coord_3d_t superposition_coords[N_ALL_NODES] = {
+            {0, 0, 0},
+            {1, 0, 0},
+            {2, 0, 0},
+            {4, 4, 0},
+    };
+    float superposition_dists_3d[N_ALL_NODES] = {0, 0, 1, 5}; // ie ROV at (1, 0, 0)
+    coord_3d_t superposition_estimate = multilaterate(superposition_coords, superposition_dists_3d, -5, -5);
+    bool superposition_test_passed = (fabsf(superposition_estimate.x - 1) < 1e-5
+                                      && fabsf(superposition_estimate.y - 0) < 1e-5
+                                      && fabsf(superposition_estimate.z - 0) < 1e-5);
+    if (superposition_test_passed){
+        printf("Multilateration superposition tests Passed\n");
+    } else {
+        printf("Multilateration superposition tests Failed\n");
+    }
+
+    bool all_tests_pass = quadrant_tests_pass && rov_collinear_test_passed &&
+                          stationary_collinear_test_passed && superposition_test_passed;
+    if (all_tests_pass){
+        printf("Multilateration all tests Passed\n\n");
+    } else {
+        printf("Multilateration tests Failed\n\n");
+    }
+
+    return all_tests_pass;
+}
